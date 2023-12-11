135942143ad1c95d35f9c9ce31e963090f974b8e
Devduttsinh <devduttchudasama1339@gmail.com>
2023-12-11 13:42:04 +0530

    added helper for try and catch

--- redis-as-cache/README.md
+++ redis-as-cache/README.md
@@ -1,11 +1,12 @@
 # Redis As Cache Implementation
 
-<hr/>
-Try out APIs provided in postman and watch the difference between first API call and all subsequent API call to it.
+---
+
+Try out APIs provided in postman and watch the difference in response time between first API call and all subsequent API call to it.
 
 [Postman Collection to Import](https://api.postman.com/collections/27828565-e7d476fb-58b4-4365-83ab-5d76684a3b7d?access_key=PMAT-01H3XQX5ZWJ6Y3X4074W76ZKTX)
 
-#### Requirements
+## Requirements
 
 Have below things installed on your local machine
 
@@ -13,6 +14,6 @@ Have below things installed on your local machine
 - [Redis](https://redis.io/download/)
 - [Postman](https://www.postman.com/downloads/)(To check the API response delay)
 
-#### Foot Note
+### Foot Note
 
 - You can change port and expiration time from [config.json](./config.json) file.
--- redis-as-cache/app.js
+++ redis-as-cache/app.js
@@ -1,31 +1,45 @@
 const Redis = require("redis");
-const redisClient = Redis.createClient();
+const redisClient = Redis.createClient(); // initiated redis client
 const express = require("express");
 const Json = require("./config.json");
 
 const app = express();
-redisClient.connect();
-module.exports = { redisClient };
-const { setOrGetCache } = require("./cacheHelper");
+redisClient.connect(); // connect
+module.exports = { redisClient }; // exporting to use it in any other files
+const { setOrGetCache } = require("./cacheHelper"); // if cache found return else set
 
 app.use(express.json());
 
-app.get("/photos", async (req, res) => {
+let help = async (req, cb) => {
   try {
+    let data = await cb(req);
+    return [data, null];
+  } catch (err) {
+    console.log(err);
+    return [null, err];
+  }
+};
+
+app.get("/photos", async (req, res) => {
+  let cb = async (req) => {
     const getPhotos = async () => {
       let photos = await fetch(`${Json.URL}`);
       return await photos.json();
     };
 
     const photos = await setOrGetCache("photos", getPhotos);
-    res.json(photos);
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
+    return photos;
+  };
+
+  let [data, err] = await help(req, cb);
+
+  if (err) res.sendStatus(500);
+
+  return res.json(data);
 });
+
 app.get("/photos/:albumId", async (req, res) => {
-  try {
+  let cb = async (req) => {
     const { albumId } = req.params;
     const getAlbumPhotos = async () => {
       let photos = await fetch(`${Json.URL}?albumId=${albumId}`);
@@ -35,15 +49,16 @@ app.get("/photos/:albumId", async (req, res) => {
       `photos?albumId=${albumId}`,
       getAlbumPhotos
     );
+    return photos;
+  };
 
-    res.json(photos);
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
+  let [data, err] = await help(req, cb);
+  if (err) res.sendStatus(500);
+  return res.json(data);
 });
+
 app.get("/photo/:id", async (req, res) => {
-  try {
+  let cb = async (req) => {
     const { id } = req.params;
 
     const getPhoto = async () => {
@@ -51,11 +66,12 @@ app.get("/photo/:id", async (req, res) => {
       return await photo.json();
     };
     const photo = await setOrGetCache(`photos:${id}`, getPhoto);
-    res.json(photo);
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
+    return photo;
+  };
+
+  let [data, err] = await help(req, cb);
+  if (err) res.sendStatus(500);
+  return res.json(data);
 });
 
 app.listen(Json.PORT, (err, data) => {
